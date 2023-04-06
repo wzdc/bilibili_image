aa5cb617b3c53b6c8d2fbfb6368cd9f3b21ed7cf
无知的错 <2067547250@qq.com>
2023-04-06 17:53:15 +0800

    Update bilibili_image.php
    修复无限重定向错误

--- bilibili_image.php
+++ bilibili_image.php
@@ -1,37 +1,45 @@
 <?php
-include('simple_html_dom.php');
-header("Access-Control-Allow-Origin:*"); //接受所有访问
-if(is_Mobile()&&$_REQUEST["mobile_search"])
+include('lzy/simple_html_dom.php');
+//header("Access-Control-Allow-Origin:*"); //接受所有访问（似乎不需要）
+if(is_Mobile()&&isset($_REQUEST["mobile_search"])&&$_REQUEST["mobile_search"])
 $q=urlencode($_REQUEST["mobile_search"]);
 else
-$q=urlencode($_REQUEST["search"]);
+$q=urlencode($_REQUEST["search"] ?? "");
 if(!$q) exit;
 $p=rand(1,30); //随机页数
 
 $url = GET("https://api.bilibili.com/x/web-interface/wbi/search/type?__refresh__=true&_extra=&context=&page=$p&page_size=1&order=totalrank&duration=&from_source=&from_spmid=333.337&platform=pc&highlight=1&single_column=0&keyword=$q&qv_id=LQu75dQ0Mz3MbQwEQa3Sgv7vLFqYEq5l&ad_resource=&source_tag=3&category_id=0&search_type=article&w_rid=9fa7196ec35041e8c2b0d41bc0b273bd&wts=1673791435");
 $arr = json_decode($url, true);
 $id=$arr["data"]["result"][0]["id"];
+if(!$id) exit;
 $data = GET("https://www.bilibili.com/read/cv$id");
-$size=rand(0,substr_count($data,"<img")-1); //随机截取一张图片
 $html = new simple_html_dom();
-$a="data-src";
-$img='https:'.$html->load($data)->find("img",$size)->$a;
+$html = $html->load($data);
+if(!$html) exit;
+for($i=0;$i<=10;$i++){ //如果十次都没有获取到指定的图片则停止
+    $size=rand(0,substr_count($data,"<img")-1); //随机截取一张图片
+    $img = 'https:' . $html->find('img', $size)->getAttribute('data-src');
+    if(getimagesize($img)[1]>=400) { //判断图片长度是否超过400（主要用来防装饰图片，会减慢响应速度）
+        //header("location: https://image.baidu.com/search/down?tn=download&ipn=dwnl&word=download&ie=utf8&fr=result&url=$img&thumburl=0"); //调用百度下载图片API绕过B站防盗链
+        header("location: https://cdn.cdnjson.com/".str_replace("https://","",$img));
+        //header("location: $img"); 
+        break;
+    }
+}
 
-if(getimagesize($img)[1]>=400) //判断图片长度是否超过400（主要用来防装饰图片）
-header("location:https://image.baidu.com/search/down?tn=download&ipn=dwnl&word=download&ie=utf8&fr=result&url=$img&thumburl=0"); //调用百度下载图片API绕过B站防盗链 header("location: $img");
-else
-header("location: ".$_SERVER["REQUEST_URI"]);
+//销毁对象
+$html->clear();
+unset($html);
 
 
 //GET
 function GET($url) {
-$headers[]  =  "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9";
-$headers[]  =  "Accept-Encoding: gzip, deflate, br";
-$headers[]  =  "Accept-Language: zh-CN,zh;q=0.9,zh-HK;q=0.8,zh-TW;q=0.7";
-$headers[]  =  "Cache-Control: max-age=0";
-$headers[]  =  "Connection: keep-alive";
-$headers[]  =  'sec-ch-ua: "Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"';
-$headers[]  =  "User-Agent:Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36";
+global $cookie;
+
+$headers[]= "user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36";
+$headers[]= "cookie:  buvid3=0; ";
+
+
 $curl = curl_init();
 curl_setopt($curl, CURLOPT_URL,$url);
 curl_setopt($curl, CURLOPT_HEADER,0);
@@ -63,4 +71,5 @@ function is_Mobile()
         return false;
     }
 }
+
 ?>
