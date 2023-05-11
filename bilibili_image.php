1e82c097c090d58eacacc74ee6fb60af901cd228
无知的错 <2067547250@qq.com>
2023-05-11 19:58:01 +0800

    Update bilibili_image.php

--- bilibili_image.php
+++ bilibili_image.php
@@ -1,24 +1,25 @@
 <?php
-include('simple_html_dom.php');
 //header("Access-Control-Allow-Origin:*"); //接受所有访问（似乎不需要）
-if(is_Mobile()&&isset($_REQUEST["mobile_search"])&&$_REQUEST["mobile_search"])
-$q=urlencode($_REQUEST["mobile_search"]);
-else
-$q=urlencode($_REQUEST["search"] ?? "");
-if(!$q) exit;
+if(is_Mobile()&&isset($_REQUEST["mobile_search"])&&$_REQUEST["mobile_search"]) {
+    $q=urlencode($_REQUEST["mobile_search"]);
+} else if(isset($_REQUEST["search"])&&$_REQUEST["search"]) {
+    $q=urlencode($_REQUEST["search"]);
+} else {
+    exit;
+}
 $p=rand(1,30); //随机页数
 
-$url = GET("https://api.bilibili.com/x/web-interface/wbi/search/type?__refresh__=true&_extra=&context=&page=$p&page_size=1&order=totalrank&duration=&from_source=&from_spmid=333.337&platform=pc&highlight=1&single_column=0&keyword=$q&qv_id=LQu75dQ0Mz3MbQwEQa3Sgv7vLFqYEq5l&ad_resource=&source_tag=3&category_id=0&search_type=article&w_rid=9fa7196ec35041e8c2b0d41bc0b273bd&wts=1673791435");
-$arr = json_decode($url, true);
+$json = GET("https://api.bilibili.com/x/web-interface/wbi/search/type?__refresh__=true&_extra=&context=&page=$p&page_size=1&order=totalrank&duration=&from_source=&from_spmid=333.337&platform=pc&highlight=1&single_column=0&keyword=$q&qv_id=LQu75dQ0Mz3MbQwEQa3Sgv7vLFqYEq5l&ad_resource=&source_tag=3&category_id=0&search_type=article&w_rid=9fa7196ec35041e8c2b0d41bc0b273bd&wts=1673791435");
+$arr = json_decode($json, true);
 $id=$arr["data"]["result"][0]["id"];
 if(!$id) exit;
-$data = GET("https://www.bilibili.com/read/cv$id");
-$html = new simple_html_dom();
-$html = $html->load($data);
-if(!$html) exit;
-for($i=0;$i<=10;$i++){ //如果十次都没有获取到指定的图片则停止
-    $size=rand(0,substr_count($data,"<img")-1); //随机截取一张图片
-    $img = 'https:' . $html->find('img', $size)->getAttribute('data-src');
+$html = GET("https://www.bilibili.com/read/cv$id");
+$mun=preg_match_all('/(?<=data-src=["\']).+?(?=["\'])/',$html,$data);
+if(--$mun) $data=$data[0];
+else exit;
+shuffle($data);
+foreach ($data as $v){
+    $img = 'https:' . $v;
     if(getimagesize($img)[1]>=400) { //判断图片长度是否超过400（主要用来防装饰图片，会减慢响应速度）
         //header("location: https://image.baidu.com/search/down?tn=download&ipn=dwnl&word=download&ie=utf8&fr=result&url=$img&thumburl=0"); //调用百度下载图片API绕过B站防盗链
         header("location: https://cdn.cdnjson.com/".str_replace("https://","",$img));
@@ -27,32 +28,24 @@ for($i=0;$i<=10;$i++){ //如果十次都没有获取到指定的图片则停止
     }
 }
 
-//销毁对象
-$html->clear();
-unset($html);
-
-
 //GET
 function GET($url) {
-global $cookie;
-
-$headers[]= "user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36";
-$headers[]= "cookie:  buvid3=0; ";
-
-
-$curl = curl_init();
-curl_setopt($curl, CURLOPT_URL,$url);
-curl_setopt($curl, CURLOPT_HEADER,0);
-curl_setopt($curl,CURLOPT_RETURNTRANSFER,true);
-curl_setopt($curl,CURLOPT_HTTPHEADER,$headers);
-curl_setopt($curl, CURLOPT_ENCODING, '');
-curl_setopt($curl,CURLOPT_RETURNTRANSFER,true);
-curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 2);
-curl_setopt($curl,CURLOPT_NOBODY,0);
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-$data = curl_exec($curl);
-curl_close($curl);
+    $headers[]= "user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36";
+    $headers[]= "cookie:  buvid3=0; ";
+    
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL,$url);
+    curl_setopt($curl, CURLOPT_HEADER,0);
+    curl_setopt($curl,CURLOPT_RETURNTRANSFER,true);
+    curl_setopt($curl,CURLOPT_HTTPHEADER,$headers);
+    curl_setopt($curl, CURLOPT_ENCODING, '');
+    curl_setopt($curl,CURLOPT_RETURNTRANSFER,true);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 2);
+    curl_setopt($curl,CURLOPT_NOBODY,0);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    $data = curl_exec($curl);
+    curl_close($curl);
     return $data;
 }
 
@@ -71,5 +64,4 @@ function is_Mobile()
         return false;
     }
 }
-
 ?>
